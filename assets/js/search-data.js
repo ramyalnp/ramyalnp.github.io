1677c24bd0ea26087228eb5eb91d4ff1e1859dbd
ramyalnp <rl882@cornell.edu>
2025-06-27 18:25:29 +0000

    Deploying to gh-pages from @ ramyalnp/ramyalnp.github.io@88fa7a45279144b32ff25f706160656fdd87069b 🚀

---
+++ assets/js/search-data.js
@@ -0,0 +1,135 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "About",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-blog",
+          title: "Blog",
+          description: "Just my thoughts",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
+        },{id: "nav-publications",
+          title: "Publications",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/Publications/";
+          },
+        },{id: "nav-projects",
+          title: "Projects",
+          description: "A growing collection of my blood sweat and tears ;)",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
+        },{id: "post-a-short-announcement",
+        
+          title: "A short announcement",
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/short-announcement/";
+          
+        },
+      },{id: "books-the-godfather",
+          title: 'The Godfather',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_godfather/";
+            },},{id: "projects-metabolic-regulation",
+          title: 'METABOLIC REGULATION',
+          description: "Understanding the human secretome for potential drug targets to treat metabolic dysfunctions",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/1_project/";
+            },},{id: "projects-autism-spectrum-disorders",
+          title: 'AUTISM SPECTRUM DISORDERS',
+          description: "Using patient-derived organoid models to study Autism Spectrum Disorders",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/2_project/";
+            },},{id: "projects-magnetic-distractor",
+          title: 'MAGNETIC DISTRACTOR',
+          description: "Applying engineering principles to build a magnetic distractor instrument to treat craniosynostosis",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/3_project/";
+            },},{id: "projects-rents-these-days",
+          title: 'RENTS THESE DAYS',
+          description: "Scouring the California housing dataset to predict the cost of a roof over my head",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/4_project/";
+            },},{id: "projects-attr-cm",
+          title: 'ATTR-CM',
+          description: "Uncovering the role of the extracellular matrix in the occurence of Amlyoid Transthyretin Cardiomyopathy",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/8_project/";
+            },},{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+        },
+      },{
+        id: 'social-inspire',
+        title: 'Inspire HEP',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-rss',
+        title: 'RSS Feed',
+        section: 'Socials',
+        handler: () => {
+          window.open("/feed.xml", "_blank");
+        },
+      },{
+        id: 'social-scholar',
+        title: 'Google Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+        },
+      },{
+        id: 'social-custom_social',
+        title: 'Custom_social',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.alberteinstein.com/", "_blank");
+        },
+      },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
